b1d7ad0cc4aecc6f39cd03a407010eb5bf8a20b3
Jeremy Johnson <jeremy4.johnson@umconnect.umt.edu>
2017-03-14 18:59:39 -0600

    no message

--- HW-6/readMe.md
+++ HW-6/readMe.md
@@ -3,5 +3,5 @@
 ##My Take
   The fact that we can now have if and then statements, with changing variables by the click of a button is pretty awesome. I wasn't in class on Wednesday but through viewing the work of my classmates it seems that its opening up a new world of artistic ability for us.
 
-  ##Issues
-  Since I wasn't in class my Var = state is screwing with my stoplight starting and stopping. Hopefully in class we can go over how to get my button to work. Since the stop light is supposed to be red with the "Do not walk hand". The Red button is supposed to be clickable that changes the light to green and giving the walking man, indicating its safe to cross the road. 
+##Issues
+  Since I wasn't in class my Var = state is screwing with my stoplight starting and stopping. Hopefully in class we can go over how to get my button to work. Since the stop light is supposed to be red with the "Do not walk hand". The Red button is supposed to be clickable that changes the light to green and giving the walking man, indicating its safe to cross the road.
--- HW-6/sketch.js
+++ HW-6/sketch.js
@@ -1,45 +1,67 @@
 var exy = 15;
 var state = false;
-var click = 10;
+var button_diam = 10;
+var button_loc_x = 333;
+var button_loc_y = 460;
 var d = 20;
 
+var person = {
+  present: false,
+  button_x: 640,
+  button_y: 30,
+  button_size: 40,
+};
+
 function setup() {
-createCanvas(700, 700);
-background(0, 204, 204);
-
-fill(224, 224, 224);
-strokeWeight(1);
-rect(325, 0, 15, 650);
-rect(250, 650, 600, 50);
-rect(0, 10, 325, 15);
-
-fill(255, 222, 0);
-strokeWeight(1);
-rect(340, 250, 45, 5);
-rect(340, 220, 45, 5);
-rect(10, 5, 36, 70);
-rect(363, 218, 49, 49);
-rect(294, 78, 36, 70);
-
-rect(340, 290, 45, 5);
-rect(340, 320, 45, 5);
-rect(363, 290, 49, 49);
-
-
-fill(0);
-rect(367, 222, 42, 42);
-rect(297, 80, 30, 65);
-rect(13, 8, 30, 65);
-rect(367, 294, 42, 42);
-
-fill(32);
-rect(0, 670, 250, 35);
-
-fill(128, 128, 128);
-rect(325, 450, 15,25);
+  createCanvas(700, 700);
 }
 
 function draw() {
+  background(0, 204, 204);
+
+  fill(224, 224, 224);
+  strokeWeight(1);
+  rect(325, 0, 15, 650);
+  rect(250, 650, 600, 50);
+  rect(0, 10, 325, 15);
+
+  fill(255, 222, 0);
+  strokeWeight(1);
+  rect(340, 250, 45, 5);
+  rect(340, 220, 45, 5);
+  rect(10, 5, 36, 70);
+  rect(363, 218, 49, 49);
+  rect(294, 78, 36, 70);
+
+  rect(340, 290, 45, 5);
+  rect(340, 320, 45, 5);
+  rect(363, 290, 49, 49);
+
+
+  fill(0);
+  rect(367, 222, 42, 42);
+  rect(297, 80, 30, 65);
+  rect(13, 8, 30, 65);
+  rect(367, 294, 42, 42);
+
+  fill(32);
+  rect(0, 670, 250, 35);
+
+  fill(128, 128, 128);
+  rect(325, 450, 15,25);
+
+// draw person present button
+ push();
+
+ if(person.present) {
+   fill(0, 255, 0);
+ } else {
+   fill(255);
+ }
+rect(person.button_x, person.button_y, person.button_size, person.button_size );
+
+pop();
+
 if (state) {
 
 fill(255, 0, 0);
@@ -55,8 +77,6 @@ ellipse(380, 250, 15, 5);
 strokeWeight(0);
 ellipse(390, 250, 20, 15);
 
-
-
 } else {
   fill(0, 204, 0);
   ellipse(28, 60, exy, exy);
@@ -71,11 +91,19 @@ ellipse(390, 250, 20, 15);
   rect(385, 310, 5, 15);
   ellipse(388, 304, 13, 13);
   }
-ellipse(333, 460, click, click);
+ellipse(butt_loc_x, butt_loc_y, butt_diam, butt_diam);
 }
 
 function mousePressed() {
-  if (dist(mouseX, mouseY, click, click) < d / 2) {
+  if (dist(mouseX, mouseY, button_loc_x, button_loc_y) < button_diam / 2) {
     state = !state;
-      }
   }
+
+if( mouseX >= person.button_x &&
+    mouseX <= person.button_x + person.button_size &&
+    mouseY >= person.button_y &&
+    mouseY <= person.button_y + person.button_size
+  )  {
+    person.present = !person.present;
+    }
+}
